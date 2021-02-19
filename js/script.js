08d6b2832db3a4b0368f66a583af2a8af0178257
happycodingpy <hugo.orlando2030@gmail.com>
2021-02-19 20:10:36 -0300

    js

--- js/script.js
+++ js/script.js
@@ -22,6 +22,13 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -41,11 +48,7 @@ btnRoll.addEventListener('click', function () {
     ).textContent = currentScore;
   } else {
     // Switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
 });
 
@@ -55,4 +58,10 @@ btnHold.addEventListener('click', function () {
 
   document.getElementById(`current-${activePlayer}`).textContent =
     scores[activePlayer];
+
+  // 2. Check if player's score is >= 100
+
+  // 3. Switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  switchPlayer();
 });
