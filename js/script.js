bdbba943e1c63768949ec197097f4d75f49cdaf4
happycodingpy <hugo.orlando2030@gmail.com>
2021-02-21 15:48:52 -0300

    js

--- js/script.js
+++ js/script.js
@@ -67,6 +67,7 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 100) {
       // Finish de game
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
